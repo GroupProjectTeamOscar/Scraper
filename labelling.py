4eb4723844f49195977ed592fa8a1b8f07f4d7df
annieon123 <ajo37@cam.ac.uk>
2017-02-17 08:29:22 +0000

    Update labelling.py

--- labelling.py
+++ labelling.py
@@ -1,21 +1,11 @@
-def depressedWords(): 
+def label(stringOfWords): 
   
     open_file = open('depressedwords.txt', 'r')
     depressed_words_list =[]
     contents = open_file.readlines()
     for i in range(len(contents)):
-         depressed_words_list.append(contents[i].strip('\n'))   
+         if contents[i].strip('\n') in stringOfWords:
+                open_file.close()
+                return 1
     open_file.close()  
-    return depressed_words_list 
-
-def firstlabel(stringOfWords): 
-    
-    #takes a string and returns true if keyphase is in the string 
-    
-    depressedWordsList = depressedWords()
-    
-    for w in depressedWordsList:
-        if w in stringOfWords: 
-            return 1
     return 0
-   
