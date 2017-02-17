5e037b847ae0f584ae1ac578b70bced00fccabaf
annieon123 <ajo37@cam.ac.uk>
2017-02-17 15:33:26 +0000

    Update spellchecking.py

--- spellchecking.py
+++ spellchecking.py
@@ -1,28 +1,29 @@
+#define in class so only called once 
+class ALLWORDS: 
+    W = Counter(words(open('big.txt', 'r').read()))
+
+    
 import re
 from collections import Counter
 
 def words(text): return re.findall(r'\w+', text)
 
-def WORDS(): 
-    output = Counter(words(open('big.txt', 'r').read()))
-    return output
-
+#find probability of word from occurances of word in large text file
 def Probability(word): 
-    W = Counter(words(open('big.txt', 'r').read()))
-    N=sum(W.values())
-    "Probability of `word`."
-    return W[word] / N
+    counter = ALLWORDS()
+    WORDS = counter.W
+    return WORDS[word]
 
+#all words reachable by 1 or 2 edits 
 def candidates(word, W): 
-    "Generate possible spelling corrections for word."
     return (known([word], W) or known(edits1(word), W) or known(edits2(word),W) or [word])
 
+#all know words from text
 def known(words, W): 
-    "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in W)
 
+#all words reachable by 1 modification
 def edits1(word):
-    "All edits that are one edit away from `word`."
     letters    = 'abcdefghijklmnopqrstuvwxyz'
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
     deletes    = [L + R[1:]               for L, R in splits if R]
@@ -31,17 +32,20 @@ def edits1(word):
     inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
+#all words reachable by 2 modifications 
 def edits2(word): 
-    "All edits that are two edits away from `word`."
     return set(e2 for e1 in edits1(word) for e2 in edits1(e1))
     
-def spellcheck(word, W): 
-    "Most probable spelling correction for word."
-    
+#best option of corrected word, due to probability key 
+def spellcheck(word, W):  
+    if known([word],W):
+        return word 
     return max(candidates(word,W), key=Probability)
 
+#spellcheck all words in text 
 def checktext(input): 
-    WORDS = Counter(words(open('big.txt', 'r').read()))
+    counter = ALLWORDS()
+    WORDS = counter.W
     text = re.sub("[^a-zA-Z]", " ", input).split()
     output = ""
     for w in text: 
@@ -49,4 +53,8 @@ def checktext(input):
         output+=w
         output+=" "
     return output
+
+
+
+
 
