c6a24ff4ce272ee9f32f21295dca3d6e21b365fc
annieon123 <ajo37@cam.ac.uk>
2017-02-23 11:28:55 +0000

    Update labelling2.py

--- labelling2.py
+++ labelling2.py
@@ -183,8 +183,7 @@ max_depth = 10
 min_size = 1
 sample_size = 1.0
 n_features = int(sqrt(len(dataset[0])-1))
-for n_trees in [1, 5, 10]:
-    scores = evaluate_algorithm(dataset, random_forest, n_folds, max_depth, min_size, sample_size, n_trees, n_features)
-    print('Trees: %d' % n_trees)
-    print('Scores: %s' % scores)
-    print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+n_trees = 5
+
+evaluate_algorithm(dataset, random_forest, n_folds, max_depth, min_size, sample_size, n_trees, n_features)
+
